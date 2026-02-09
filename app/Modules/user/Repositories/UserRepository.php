df647e03220bde7049f9f7b3218b3fd2e78a712a
Azuki.I <azuking69@hotmail.co.jp>
2026-02-09 21:30:32 +0900

    26.02.09 made UserRepository.php

---
+++ app/Modules/user/Repositories/UserRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Modules\User\Repository;
+
+// Base
+use App\Common\Base\BaseRepository;
+
+// 継承
+class UserRepository extends BaseRepository{
+    // テーブル名
+    protected string $table = 'users';
+
+    // DBを UserRepository -> BaseRepository
+    public function __construct(Database $db){
+        parent:: __construct($db);
+    }
+
+    // メール重複確認
+    public function emailExists(string $email): bool{
+        return $this -> exists(['email' => $email]);
+    }
+}
