893d87a3d7d1d16bc88a1e52f1e3c5b124b3932c
Azuki.I <azuking69@hotmail.co.jp>
2026-02-17 18:21:47 +0900

    26.02.17 made omamori.php

---
+++ routes/modules/omamori.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Modules\Omamori\Controllers\OmamoriController;
+
+global $router;
+
+// 오마모리 생성
+$router -> post('api/omamori', function($request){
+    return (new OmamoriController()) -> store($request);
+}, 'omamoris.store');
