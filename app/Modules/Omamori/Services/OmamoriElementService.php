2aca4d429579affbfcc6cc009281646d8a6d9de1
Azuki.I <azuking69@hotmail.co.jp>
2026-02-23 17:23:18 +0900

    26.02.23 made stamp element

--- app/Modules/Omamori/Services/OmamoriElementService.php
+++ app/Modules/Omamori/Services/OmamoriElementService.php
@@ -6,18 +6,21 @@ namespace App\Modules\Omamori\Services;
 use App\Common\Base\BaseService;
 use App\Core\Database;
 use App\Modules\Auth\Services\AuthService;
+use App\Modules\File\Repositories\FileRepository;
 use App\Modules\Omamori\Repositories\OmamoriRepository;
 use App\Modules\Omamori\Repositories\OmamoriElementRepository;
 
 
 // 상속
 class OmamoriElementService extends BaseService{
+    protected FileRepository $fileRepository;
     protected OmamoriRepository $omamoriRepository;
     protected OmamoriElementRepository $omamoriElementRepository;
 
     public function __construct()
     {
         $db = new Database();
+        $this -> fileRepository = new FileRepository($db);
         $this -> omamoriRepository = new OmamoriRepository($db);
         $this -> omamoriElementRepository = new OmamoriElementRepository($db);
     }
@@ -39,6 +42,8 @@ class OmamoriElementService extends BaseService{
             throw new \InvalidArgumentException('Invalid input');
         }
 
+       
+
         // 오마모리 요소
         $type = $input['type'];
         $layer = $input['layer'];
@@ -47,6 +52,17 @@ class OmamoriElementService extends BaseService{
 
         if(!is_array($props) || !is_array($transform)){
             throw new \InvalidArgumentException('props and transform must be object');
+        }
+
+        if($type === 'stamp'){
+            if(!isset($props['asset_key'])){
+                throw new \InvalidArgumentException('asset_key required');
+            }
+        }
+
+        $file = $this -> fileRepository -> findByFileKey($props['asset_key']);
+        if(!$file){
+            throw new \RuntimeException('Invalid asset_key');
         }
 
         // 저장
