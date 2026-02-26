9bdc0298deff0ad66e3f4bc989f7467cadc67d6e
Azuki.I <azuking69@hotmail.co.jp>
2026-02-26 12:18:52 +0900

    26.02.26 fixed omamori.php

--- routes/modules/omamori.php
+++ routes/modules/omamori.php
@@ -62,5 +62,5 @@ $router -> patch('/api/omamoris/[i:omamoriId]/elements/[i:elementId]', function(
 
 // 오마모리 요소 삭제
 $router -> delete('/api/omamoris/[i:omamoriId]/elements/[i:elementId]', function($request){
-    return (new ElementController()) -> deleteElement($request);
-}, 'omamorisElements.delete');
+    return (new ElementController()) -> destroy($request);
+}, 'omamoriElements.delete');
