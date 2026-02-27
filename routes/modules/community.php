128f995a7b7d16aa55cb8e79a105e475d47d6997
Azuki.I <azuking69@hotmail.co.jp>
2026-02-27 20:16:50 +0900

    26.02.27 fixed community.php

--- routes/modules/community.php
+++ routes/modules/community.php
@@ -6,6 +6,6 @@ use App\Modules\Community\Controllers\PostController;
 global $router;
 
 // 게시글 작성
-$router -> post('/api/posts/', function($request){
+$router -> post('/api/posts', function($request){
     return (new PostController()) -> store($request);
 }, 'omamoris.community.store');
