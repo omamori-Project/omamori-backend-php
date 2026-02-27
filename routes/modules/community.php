9d67f88b8367318b684f0f9c40bb7999a967ca7a
Azuki.I <azuking69@hotmail.co.jp>
2026-02-27 21:21:35 +0900

    26.02.27 insert show of community.php

--- routes/modules/community.php
+++ routes/modules/community.php
@@ -9,3 +9,8 @@ global $router;
 $router -> post('/api/posts', function($request){
     return (new PostController()) -> store($request);
 }, 'omamoris.community.store');
+
+// 전체 게시글 목록 조회 (공개 피드)
+$route -> get('/api/posts', function($request){
+    return (new PostController()) -> show($request);
+}, 'omamoris.community.show');
