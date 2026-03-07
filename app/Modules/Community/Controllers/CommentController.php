b8c2028110a3e1344344d442d1b4b90fa5423fe8
Azuki.I <azuking69@hotmail.co.jp>
2026-03-07 16:20:52 +0900

    26.03.07 inserted update of CommentController.php

--- app/Modules/Community/Controllers/CommentController.php
+++ app/Modules/Community/Controllers/CommentController.php
@@ -79,4 +79,27 @@ class CommentController extends BaseController{
             return ErrorHandler:: handle($e);
         }
     }
+
+
+    // 댓글 수정
+    public function update(Request $request): Response{
+        try{
+            // commentId 검증
+            $commentId = (int)$request -> param('commentId', 0);
+            if($commentId <= 0){
+                return $this -> error('Invalid commentId');
+            }
+
+            // body 입력값 받기
+            $input = $request -> input();
+
+            // 서비스 호출
+            $result = $this -> commentService -> updateComment($commentId, $input);
+
+            return $this -> success($result, 'Updated', 200);
+
+        }catch(\Exception $e){
+            return ErrorHandler:: handle($e);
+        }
+    }
 }
