abe7c5f957d5379de0ffbc90d5285de6a2af40a0
Azuki.I <azuking69@hotmail.co.jp>
2026-03-09 17:34:27 +0900

    26.03.09 made ShareController.php

--- app/Modules/Public/Controllers/ShareController.php
+++ app/Modules/Public/Controllers/ShareController.php
@@ -4,9 +4,34 @@ namespace App\Modules\Public\Controllers;
 
 // import
 use App\Common\Base\BaseController;
+use App\Common\Exceptions\ErrorHandler;
+use App\Core\Request;
+use App\Core\Response;
+use App\Modules\Public\Services\ShareService;
 
 
 // 상속
 class ShareController extends BaseController{
-    
+    protected ShareService $shareService;
+
+    public function __construct(){
+        $this -> shareService = new ShareService();
+    }
+
+    // 외부 공유용 오마모리 조회
+    public function show(Request $request): Response{
+        try {
+            $token = (string) $request -> param('token', '');
+
+            if ($token === '') {
+                return $this -> error('Invalid token');
+            }
+
+            $result = $this -> shareService -> showByToken($token);
+            return $this -> success($result, 'OK', 200);
+
+        } catch (\Exception $e) {
+            return ErrorHandler:: handle($e);
+        }
+    }
 }
