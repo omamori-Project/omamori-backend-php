37ad85c6752452e9b878046169e40f607a8e7e4e
Azuki.I <azuking69@hotmail.co.jp>
2026-03-12 21:36:24 +0900

    26.03.12 made frame admin

---
+++ app/Modules/Custom/Repositories/AdminFrameRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Modules\Custom\Repositories;
+
+// import
+use App\Common\Base\BaseRepository;
+use App\Core\Database;
+
+
+// 상속
+class AdminFrameRepository extends BaseRepository{
+    
+}
