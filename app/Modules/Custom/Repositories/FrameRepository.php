406afe5c9b7774c97b473c4b9ff00e5a5f5fd68c
Azuki.I <azuking69@hotmail.co.jp>
2026-03-12 21:14:09 +0900

    26.03.12 inserted removeFrame of FrameRepository.php

--- app/Modules/Custom/Repositories/FrameRepository.php
+++ app/Modules/Custom/Repositories/FrameRepository.php
@@ -32,4 +32,13 @@ class FrameRepository extends BaseRepository{
     public function findByFrameKey(string $frameKey): ?array{
         return $this -> findOneBy(['frame_key' => $frameKey]);
     }
+
+
+    // 프레임 적용 해제
+    public function removeFrame(int $omamoriId): bool{
+        return $this -> update($omamoriId, [
+            'applied_frame_id' => null,
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+    }
 }
