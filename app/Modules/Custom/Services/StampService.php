2066921952a3b7386b183759ca535936465a6341
Azuki.I <azuking69@hotmail.co.jp>
2026-03-13 00:03:00 +0900

    26.03.12 fixed codes of StampService.php

--- app/Modules/Custom/Services/StampService.php
+++ app/Modules/Custom/Services/StampService.php
@@ -16,6 +16,7 @@ class StampService extends BaseService{
         $this -> stampRepository = new StampRepository();
     }
 
+    // 스탬프 목록 조회 + 필터링
     public function getStampList(array $query): array{
         return $this -> stampRepository -> getList($query);
     }
