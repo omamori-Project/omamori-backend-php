cfd8f1a6906049db7604885fa3631f4e8312c3a5
Azuki.I <azuking69@hotmail.co.jp>
2026-03-13 00:02:22 +0900

    26.03.13 made StampService.php

--- app/Modules/Custom/Services/StampService.php
+++ app/Modules/Custom/Services/StampService.php
@@ -4,10 +4,19 @@ namespace App\Modules\Custom\Services;
 
 // import
 use App\Common\Base\BaseService;
-
+use App\Modules\Custom\Repositories\StampRepository;
 
 
 // 상속
 class StampService extends BaseService{
-    
+    protected StampRepository $stampRepository;
+
+    public function __construct()
+    {
+        $this -> stampRepository = new StampRepository();
+    }
+
+    public function getStampList(array $query): array{
+        return $this -> stampRepository -> getList($query);
+    }
 }
