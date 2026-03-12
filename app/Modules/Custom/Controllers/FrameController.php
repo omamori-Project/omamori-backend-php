632da90d2f768a36745e5d037da778aa0f7e5bdc
Azuki.I <azuking69@hotmail.co.jp>
2026-03-12 17:43:55 +0900

    26.03.12 made FrameController.php

--- app/Modules/Custom/Controllers/FrameController.php
+++ app/Modules/Custom/Controllers/FrameController.php
@@ -4,9 +4,30 @@ namespace App\Modules\Custom\Controllers;
 
 // import
 use App\Common\Base\BaseController;
+use App\Common\Exceptions\ErrorHandler;
+use App\Core\Request;
+use App\Core\Response;
+use App\Modules\Custom\Services\FrameService;
 
 
 // 상속
 class FrameController extends BaseController{
-    
+    protected FrameService $frameService;
+
+    public function __construct()
+    {
+        $this -> frameService = new FrameService();
+    }
+
+    // 유저용 프레임 목록
+    public function index(Request $request): Response{
+        try {
+            $query = $request -> query();
+            $result = $this -> frameService -> getFrames($query);
+            return $this->success($result, 'OK', 200);
+
+        } catch (\Exception $e) {
+            return ErrorHandler:: handle($e);
+        }
+    }
 }
