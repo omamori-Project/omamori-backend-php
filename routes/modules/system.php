f7b1d40a5fe8f7e8ea9ff2ad3dc9c787086ad696
Azuki.I <azuking69@hotmail.co.jp>
2026-03-14 01:27:32 +0900

    26.03.14 fixed codes of system.php

--- routes/modules/system.php
+++ routes/modules/system.php
@@ -33,6 +33,6 @@ $router -> post('/api/files', function($request){
 }, 'omamoris.file.upload');
 
 // 파일 삭제
-$router -> post('/api/files/[i:fileId]', function($request){
+$router -> post('/api/files/{fileId}', function($request){
     return (new FileController()) -> destroy($request);
 }, 'omamoris.file.destroy');
