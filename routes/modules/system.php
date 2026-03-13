472937c18ed0aba5e67a2ef190502177a1a05581
Azuki.I <azuking69@hotmail.co.jp>
2026-03-13 22:31:14 +0900

    26.03.12 inserted show of system.php

--- routes/modules/system.php
+++ routes/modules/system.php
@@ -10,3 +10,8 @@ global $router;
 $router -> post('/api/render/omamori', function($request){
     return (new RenderController()) -> create($request);
 }, 'omamoris.render.create');
+
+// 렌더 결과 조회
+$router -> get('/api/render/{renderCode}', function($request){
+    return (new RenderController()) -> show($request);
+}, 'omamoris.render.show');
