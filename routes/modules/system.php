77fd5cba83b1f891c2c00199bbda509a18e00050
Azuki.I <azuking69@hotmail.co.jp>
2026-03-13 23:25:08 +0900

    26.03.13 inserted myHistory of system.php

--- routes/modules/system.php
+++ routes/modules/system.php
@@ -11,6 +11,11 @@ $router -> post('/api/render/omamori', function($request){
     return (new RenderController()) -> create($request);
 }, 'omamoris.render.create');
 
+// 내 렌더 히스토리
+$router -> get('/api/renders/me', function($request){
+    return (new RenderController()) -> myHistory($request);
+}, 'omamoris.render.myHistory');
+
 // 렌더 결과 조회
 $router -> get('/api/renders/[*:renderCode]', function($request){
     return (new RenderController()) -> show($request);
